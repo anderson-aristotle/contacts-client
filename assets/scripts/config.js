dd22b8779d3e5d4610f51e96d036cd337f15cba5
rex <anderson.aristotle@gmail.com>
2019-03-18 17:04:56 -0400

    heroku - server correction

--- assets/scripts/config.js
+++ assets/scripts/config.js
@@ -2,7 +2,7 @@
 
 let apiUrl
 const apiUrls = {
-  production: 'https://git.heroku.com/stark-plateau-contacts.git',
+  production: 'ttps://stark-plateau-contacts.herokuapp.com/',
   development: 'http://localhost:4741'
 }
 
