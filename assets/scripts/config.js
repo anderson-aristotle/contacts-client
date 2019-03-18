a6c008841f0b08ec5d1f5097eef587977fb9f10d
rex <anderson.aristotle@gmail.com>
2019-03-18 16:55:57 -0400

    test backend

--- assets/scripts/config.js
+++ assets/scripts/config.js
@@ -2,7 +2,7 @@
 
 let apiUrl
 const apiUrls = {
-  production: 'https://stark-plateau-12305.herokuapp.com',
+  production: 'https://git.heroku.com/stark-plateau-contacts.git',
   development: 'http://localhost:4741'
 }
 
